b8610c9ae6d2952816307841e564a92da7bf876b
islamelhady <i.elhady44@gmail.com>
2021-02-02 13:56:55 +0200

    convert user class

--- app/src/main/java/com/elhady/convertingtokotlin/User.java
+++ app/src/main/java/com/elhady/convertingtokotlin/User.java
@@ -1,29 +1,3 @@
 package com.elhady.convertingtokotlin;
 
-public class User {
-    @Nullable
-    private String firstName;
-    @Nullable
-    private String lastName;
-
-    public User(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-}
+data class User(var firstName: String?, var lastName: String?)
