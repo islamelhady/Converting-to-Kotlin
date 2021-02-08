b555e2cd8460955ef96418eab7be3406da1ab988
islamelhady <i.elhady44@gmail.com>
2021-02-08 17:17:50 +0200

    create the sleep night entity

--- app/src/main/java/com/elhady/trackapp/database/SleepNight.kt
+++ app/src/main/java/com/elhady/trackapp/database/SleepNight.kt
@@ -1,7 +1,24 @@
 package com.elhady.trackapp.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 /**
  * Created by islam elhady on 07-Feb-21.
  */
-class SleepNight {
-}
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight(
+
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
+
+    @ColumnInfo(name = "sleep_quality")
+    var sleepQuality: Int = -1
+)
