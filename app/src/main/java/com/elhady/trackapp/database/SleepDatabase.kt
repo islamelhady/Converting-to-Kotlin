b29f280c1cee2bfa85ef4fd71178583417937ad2
islamelhady <i.elhady44@gmail.com>
2021-02-10 02:46:07 +0200

    create Room database

--- app/src/main/java/com/elhady/trackapp/database/SleepDatabase.kt
+++ app/src/main/java/com/elhady/trackapp/database/SleepDatabase.kt
@@ -1,7 +1,39 @@
 package com.elhady.trackapp.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
 /**
  * Created by islam elhady on 07-Feb-21.
  */
-class SleepDatabase {
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase : RoomDatabase() {
+
+    abstract val sleepDatabaseDao: SleepDatabaseDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: SleepDatabase? = null
+
+        fun getInstance(context: Context): SleepDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SleepDatabase::class.java,
+                        "sleep_history_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
