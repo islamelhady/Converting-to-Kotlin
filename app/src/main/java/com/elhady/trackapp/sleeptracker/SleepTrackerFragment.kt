81294af6d0e7bbac69f03aae0840b56c3ef9ddd4
islamelhady <i.elhady44@gmail.com>
2021-02-11 07:45:03 +0200

    Add a view model

--- app/src/main/java/com/elhady/trackapp/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/elhady/trackapp/sleeptracker/SleepTrackerFragment.kt
@@ -6,7 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.elhady.trackapp.R
+import com.elhady.trackapp.database.SleepDatabase
 import com.elhady.trackapp.databinding.FragmentSleepTrackerBinding
 
 
@@ -25,6 +27,19 @@ class SleepTrackerFragment : Fragment() {
         val binding: FragmentSleepTrackerBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_sleep_tracker, container, false)
 
+        val application = requireNotNull(this.activity).application
+
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        val viewModelFactory = SleepTrackerViewModelFactory(dataSource, application)
+
+        val sleepTrackerViewModel =
+            ViewModelProvider(
+                this, viewModelFactory).get(SleepTrackerViewModel::class.java)
+
+        binding.setLifecycleOwner(this)
+
+        binding.sleepTrackerViewModel = sleepTrackerViewModel
         return binding.root
     }
 
