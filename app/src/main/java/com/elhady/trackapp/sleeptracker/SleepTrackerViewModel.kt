9609f0e0e20a62e68212df1ccee73e0a2bee4395
islamelhady <i.elhady44@gmail.com>
2021-02-11 08:29:25 +0200

    Set up coroutines for database operations

--- app/src/main/java/com/elhady/trackapp/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/elhady/trackapp/sleeptracker/SleepTrackerViewModel.kt
@@ -2,7 +2,11 @@ package com.elhady.trackapp.sleeptracker
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.elhady.trackapp.database.SleepDatabaseDao
+import com.elhady.trackapp.database.SleepNight
+import kotlinx.coroutines.launch
 
 /**
  * Created by islam elhady on 07-Feb-21.
@@ -11,4 +15,24 @@ class SleepTrackerViewModel(
     val database: SleepDatabaseDao,
     application: Application
 ) : AndroidViewModel(application) {
+
+    private val tonight = MutableLiveData<SleepNight?>()
+
+    init {
+        initializeTonight()
+    }
+
+    private fun initializeTonight() {
+        viewModelScope.launch {
+            tonight.value = getTonightFromDatabase()
+        }
+    }
+
+    private suspend fun getTonightFromDatabase(): SleepNight? {
+        var night = database.getTonight()
+        if (night?.endTimeMilli != night?.startTimeMilli) {
+            night = null
+        }
+        return night
+    }
 }
