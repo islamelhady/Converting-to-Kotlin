1286b4456e0471fb21eeffb1b6988f9178ed2711
islamelhady <i.elhady44@gmail.com>
2021-02-13 09:49:55 +0200

    Add click handler for the clear button

--- app/src/main/java/com/elhady/trackapp/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/elhady/trackapp/sleeptracker/SleepTrackerViewModel.kt
@@ -69,4 +69,15 @@ class SleepTrackerViewModel(
     private suspend fun update(night: SleepNight){
         database.update(night)
     }
+
+    fun onClear() {
+        viewModelScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    suspend fun clear() {
+        database.clear()
+    }
 }
